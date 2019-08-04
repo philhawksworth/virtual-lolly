bb414f06dc1f719541abdce614d77983fdb2cd71
Phil Hawksworth <phil@netlify.com>
2019-08-04 13:24:53 +0100

    forward to 404

--- src/functions/getLolly.js
+++ src/functions/getLolly.js
@@ -16,29 +16,19 @@ exports.handler = (event, context, callback) => {
     q.Get(q.Match(q.Index("lolly_by_path"), path))
   ).then((response) => {
 
-    console.log('status :', response.requestResult.statusCode);
-
-    // if not found, redirect to the not found page
-    if(response.requestResult.statusCode == 404) {
-      return callback(null, {
-        statusCode: 301,
-        headers: {
-          Location: 'https://vlolly.netlify.com/melted',
-        }
-      });
-    } else {
-      // if found return a view
-      return callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(response.data)
-      });
-    }
+    // if found return a view
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    });
 
   }).catch((error) => {
-    console.log("error", error);
     return callback(null, {
-      statusCode: 400,
-      body: JSON.stringify(error)
+      statusCode: 301,
+      body: "Not found",
+      headers: {
+        Location: 'https://vlolly.netlify.com/melted',
+      }
     });
   });
 
