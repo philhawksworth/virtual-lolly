6559dfd7c3af5b20d1941c79bd173b0ae3256b76
Phil Hawksworth <phil@netlify.com>
2019-08-09 01:03:16 -0500

    use referer to get path

--- src/functions/showLolly.js
+++ src/functions/showLolly.js
@@ -11,9 +11,16 @@ const client = new faunadb.Client({
 
 exports.handler = (event, context, callback) => {
 
-  console.log('id :', event.queryStringParameters.id);
 
-  const path = event.queryStringParameters.id.replace("/", "");
+  if(event.queryStringParameters.id) {
+    console.log('id :', event.queryStringParameters.id.replace("/", ""));
+    var path = event.queryStringParameters.id.replace("/", "");
+  } else {
+    console.log('lolly :', event.headers.referer.split('/lolly/')[1].replace("/", ""));
+    var path = event.headers.referer.split('/lolly/')[1].replace("/", "");
+  }
+
+  console.log('path :', path);
 
   client.query(
     q.Get(q.Match(q.Index("lolly_by_path"), path))
