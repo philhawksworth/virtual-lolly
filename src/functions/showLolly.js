2f59a283432ce6406f5ee6db7bdd0fccdde22076
Phil Hawksworth <phil@netlify.com>
2019-08-13 13:10:03 +0100

    Better comments for clarity

--- src/functions/showLolly.js
+++ src/functions/showLolly.js
@@ -4,25 +4,19 @@ const pageTemplate = require('./lollyTemplate.js');
 
 require('dotenv').config();
 
+// setup and auth the Fauna DB client
 const q = faunadb.query;
 const client = new faunadb.Client({
   secret: process.env.FAUNADB_SERVER_SECRET
 });
 
+
 exports.handler = (event, context, callback) => {
 
-  console.log('event :', event);
-  var path = event.queryStringParameters.id.replace("/", "");
+  // get the lolly ID from the request
+  const path = event.queryStringParameters.id.replace("/", "");
 
-  // if(event.queryStringParameters.id) {
-  //   console.log('id :', event.queryStringParameters.id.replace("/", ""));
-  // } else if(event.headers.referer) {
-  //   console.log('lolly :', event.headers.referer.split('/lolly/')[1].replace("/", ""));
-  //   var path = event.headers.referer.split('/lolly/')[1].replace("/", "");
-  // }
-
-  console.log('path :', path);
-
+  // find the lolly data in the DB
   client.query(
     q.Get(q.Match(q.Index("lolly_by_path"), path))
   ).then((response) => {
@@ -32,10 +26,13 @@ exports.handler = (event, context, callback) => {
     // if found return a view
     return callback(null, {
       statusCode: 200,
-      body:  pageTemplate(response.data)
+      body: pageTemplate(response.data)
     });
 
   }).catch((error) => {
+
+    // not found or an error, send to the generic error page
+    console.log('Error:', error);
     return callback(null, {
       body: JSON.stringify(error),
       statusCode: 301,
