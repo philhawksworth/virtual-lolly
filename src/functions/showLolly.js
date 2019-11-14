ac0b6ddbbf560a4560774b8889f377b7343e5a26
Phil Hawksworth <phil@netlify.com>
2019-11-14 11:31:31 -0500

    syntax error fix

--- src/functions/showLolly.js
+++ src/functions/showLolly.js
@@ -14,36 +14,39 @@ exports.handler = (event, context, callback) => {
   // get the lolly ID from the request
   try {
     const path = event.queryStringParameters.id.replace("/", "");
+
+    // find the lolly data in the DB
+    client.query(
+      q.Get(q.Match(q.Index("lolly_by_path"), path))
+    ).then((response) => {
+
+      // if found return a view
+      return callback(null, {
+        statusCode: 200,
+        body: pageTemplate(response.data)
+      });
+
+    }).catch((error) => {
+
+      // not found or an error, send to the generic error page
+      console.log('Error:', error);
+      return callback(null, {
+        body: JSON.stringify(error),
+        statusCode: 301,
+        headers: {
+          Location: `/melted/index.html`,
+        }
+      });
+    });
+
   }
   catch (error) {
     console.log('Error getting query paramters :', error);
     return callback(error, {
       body: JSON.stringify(error),
-      statusCode: 500,
+      statusCode: 500
+    })
   }
 
-  // find the lolly data in the DB
-  client.query(
-    q.Get(q.Match(q.Index("lolly_by_path"), path))
-  ).then((response) => {
-
-    // if found return a view
-    return callback(null, {
-      statusCode: 200,
-      body: pageTemplate(response.data)
-    });
-
-  }).catch((error) => {
-
-    // not found or an error, send to the generic error page
-    console.log('Error:', error);
-    return callback(null, {
-      body: JSON.stringify(error),
-      statusCode: 301,
-      headers: {
-        Location: `/melted/index.html`,
-      }
-    });
-  });
 
 }
